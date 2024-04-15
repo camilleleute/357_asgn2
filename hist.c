b81237a407e4a4a5b3c83bd4ab6274ac9e3b148b
camilleleute <117230256+camilleleute@users.noreply.github.com>
2024-04-15 12:55:01 -0700

    hist.c

---
+++ hist.c
@@ -0,0 +1,15 @@
+/* Call histhelp to convert data into a histogram */
+
+#include <stdio.h>
+#include "HistHelp.h"
+
+
+int main(void) {
+    
+    while (scanf("%s", &line) != EOF) {
+    
+    }
+
+    return 0;
+}
+
